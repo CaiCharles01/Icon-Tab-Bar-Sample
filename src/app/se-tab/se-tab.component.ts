6f84ae02f3cb80c2299623a0f7a7f61710acd5ed
i525969 <charles.cai01@sap.com>
2024-03-19 14:09:36 +0800

    function works

--- src/app/se-tab/se-tab.component.ts
+++ src/app/se-tab/se-tab.component.ts
@@ -1,16 +1,19 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {IconTabBarComponent, TabConfig, TextTypePopoverComponent} from '@fundamental-ngx/platform/icon-tab-bar';
 import {cloneDeep} from 'lodash-es';
 import {textTypeConfig} from '../config-for-examples/long-icon-type-config';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-se-tab',
   standalone: true,
-  imports: [IconTabBarComponent, TextTypePopoverComponent],
+  imports: [IconTabBarComponent, TextTypePopoverComponent, AsyncPipe],
   templateUrl: './se-tab.component.html',
   styleUrl: './se-tab.component.css'
 })
 export class SeTabComponent implements OnInit, OnChanges {
+  @ViewChild('fdpIconTabBar') _fdpIconTabBar: IconTabBarComponent;
+
   @Input()
   textTypeLayoutMode: 'row' | 'column' = 'row';
 
@@ -33,12 +36,11 @@ export class SeTabComponent implements OnInit, OnChanges {
   extraItems: TabConfig[];
 
   ngOnInit(): void {
-    console.log(this.enableTabReordering);
+    this.selectedTab = this.items[0];
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['numTabsDisplayed']) {
-      console.log('debugging', this.numTabsDisplayed);
       if (this.numTabsDisplayed) {
         this.items = cloneDeep(textTypeConfig).slice(0, this.numTabsDisplayed);
         this.extraItems = cloneDeep(textTypeConfig).slice(this.numTabsDisplayed);
@@ -49,6 +51,20 @@ export class SeTabComponent implements OnInit, OnChanges {
   }
 
   onTabSelect(event: any): void {
+    this.selectedTab = event;
+  }
+
+  onMoreTabsClick(event: any): void {
+    console.log('onMoreTabsClick', event);
+    const clickTabIndex = this.extraItems.findIndex((item) => item.label === event.label);
+    if (clickTabIndex === -1) {
+      throw new Error('Tab item not found');
+    }
+    this.extraItems[clickTabIndex] = this.items[this.items.length - 1];
+    const newItems = cloneDeep(this.items);
+    newItems[newItems.length - 1] = event;
+    event.active = true;
+    this.items = newItems;
     this.selectedTab = event;
   }
 }
